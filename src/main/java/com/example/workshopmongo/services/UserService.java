60ce986e913f235d9c3759b854ea4ddf166e78b7
Jonathan Moraes <jonathan.pmoraes@senacsp.edu.br>
2022-11-22 22:06:57 -0300

    Tratando excecoes

--- src/main/java/com/example/workshopmongo/services/UserService.java
+++ src/main/java/com/example/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.example.workshopmongo.services;
 
 import com.example.workshopmongo.domain.User;
 import com.example.workshopmongo.repository.UserRepository;
+import com.example.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(()-> new ObjectNotFoundException("Object not found!"));
     }
 }
